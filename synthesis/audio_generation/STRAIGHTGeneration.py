3f9bbd9352a4044812f5da2e92f264df8c5df6bc
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2016-10-13 15:38:09 +0200

    Parameter conversion more global so in its specific file

--- synthesis/audio_generation/STRAIGHTGeneration.py
+++ synthesis/audio_generation/STRAIGHTGeneration.py
@@ -26,47 +26,7 @@ from threading import Thread
 
 from shutil import copyfile # For copying files
 
-
-###############################################################################
-# Utils
-###############################################################################
-class ParameterConversion(Thread):
-    def __init__(self, conf, out_path, base, logger):
-        Thread.__init__(self)
-        self.logger = logger
-        self.conf = conf
-        self.out_path = out_path
-        self.base = base
-
-        self.SOPR = "sopr"
-        self.MGC2SP = "mgc2sp"
-
-    def run(self):
-        # bap => aperiodicity
-        for cur_stream in self.conf.STREAMS:
-            if cur_stream["kind"] == "lf0":
-                # lf0 => f0
-                cmd = '%s -magic -1.0E+10 -EXP -MAGIC 0.0 %s/%s.lf0' % \
-                  (self.SOPR, self.out_path, self.base)
-                with open('%s/%s.f0' % (self.out_path, self.base), 'w') as f:
-                    subprocess.call(cmd.split(), stdout=f)
-            elif cur_stream["kind"] == "bap":
-                cmd = '%s -a %f -g 0 -m %d -l 2048 -o 0 %s/%s.bap' % \
-                  (self.MGC2SP, self.conf.FREQWARPING, cur_stream["order"], self.out_path, self.base)
-                with open('%s/%s.ap' % (self.out_path, self.base), 'w') as f:
-                    subprocess.call(cmd.split(), stdout=f)
-            elif cur_stream["kind"] == "mgc":
-                # mgc => spectrum TODO
-                cmd = '%s -a %f -g %f -m %d -l 2048 -o 2 %s/%s.mgc' % \
-                  (self.MGC2SP, self.conf.FREQWARPING, cur_stream['parameters']['gamma'], cur_stream["order"], self.out_path, self.base)
-                with open('%s/%s.sp' % (self.out_path, self.base), 'w') as f:
-                    subprocess.call(cmd.split(), stdout=f)
-
-        # Clean [TODO: do with options]
-        os.remove('%s/%s.lf0' % (self.out_path, self.base))
-        os.remove('%s/%s.mgc' % (self.out_path, self.base))
-        os.remove('%s/%s.bap' % (self.out_path, self.base))
-        os.remove('%s/%s.dur' % (self.out_path, self.base))
+from parameterconversion import ParameterConversion
 
 ###############################################################################
 # Functions
