14d9dfb38a3d5f1f464f69e0e383cc64c9f55665
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2017-01-08 16:31:21 +0100

    minor

--- synthesis/rendering/emarenderer.py
+++ synthesis/rendering/emarenderer.py
@@ -33,7 +33,7 @@ from rendering.emautils import *
 ###############################################################################
 # Functions
 ###############################################################################
-class EMRenderer:
+class EMARenderer:
     def __init__(self, conf, out_handle, logger, is_parallel, preserve):
         self.conf = conf
         self.logger = logger
