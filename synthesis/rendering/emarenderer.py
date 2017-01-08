aac56c65e8560bb9049d53aae48b885460f1a202
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2017-01-08 15:36:20 +0100

    try to inheritate instead of redefine (needs to be tested)

--- synthesis/rendering/emarenderer.py
+++ synthesis/rendering/emarenderer.py
@@ -33,7 +33,7 @@ from rendering.emautils import *
 ###############################################################################
 # Functions
 ###############################################################################
-class EMRendere:
+class EMRenderer:
     def __init__(self, conf, out_handle, logger, is_parallel, preserve):
         self.conf = conf
         self.logger = logger
